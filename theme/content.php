22e0b3076e2f2b6755720eebc2b6671de377070a
windyakin <windyakin@gmail.com>
2014-01-11 20:37:49 +0900

    アイキャッチを指定したらアイキャッチするようにしたい
    また戻すかもしれない

--- theme/content.php
+++ theme/content.php
@@ -13,6 +13,9 @@
 	</div>
 	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<div class="entry-content">
+		<?php if ( has_post_thumbnail()) : ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('large'); ?></a>
+		<?php endif; ?>
 		<?php the_content('続きを読む &raquo;'); ?>
 	</div>
 	<div class="entry-meta">
