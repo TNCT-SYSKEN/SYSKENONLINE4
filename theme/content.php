3454141d33d6a941f8c48d7fcd72c9dfa0a10ae9
windyakin <windyakin@gmail.com>
2014-01-14 21:50:49 +0900

    fix

--- theme/content.php
+++ theme/content.php
@@ -14,7 +14,7 @@
 	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<div class="entry-content">
 		<?php if ( has_post_thumbnail()) : ?>
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('eye-catch'); ?></a>
+		<div class="thumbnail"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('eye-catch'); ?></a></div>
 		<?php endif; ?>
 		<?php the_content('続きを読む &raquo;'); ?>
 	</div>
