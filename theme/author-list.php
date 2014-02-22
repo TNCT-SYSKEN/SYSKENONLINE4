ea8a862c9ce8325efb438bbf7285c71f75f83804
windyakin <windyakin@gmail.com>
2014-02-22 17:19:40 +0900

    うまく行かなかったので修正

--- theme/author-list.php
+++ theme/author-list.php
@@ -11,10 +11,12 @@ get_header();?>
 
 		<!-- メインバー(左) ここから -->
 		<div id="main" class="col_9">
-			<div class="abox shadow profile">
+			<?php while (have_posts()) : the_post() ?>
+			<div class="abox shadow">
 				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			</div>
+			<?php endwhile; ?>
 
 			<?php
 				/* 入学年(enterYear)をキーにソートする */
