beb399d03f475a233899a7dc293238fda3f023e1
windyakin <windyakin@gmail.com>
2014-03-18 22:06:43 +0900

    記事に部員のプロフィールを表示するようにした

--- theme/content.php
+++ theme/content.php
@@ -18,6 +18,55 @@
 		<?php endif; ?>
 		<?php the_content('続きを読む &raquo;'); ?>
 	</div>
+
+	<?php if(is_single()): ?>
+	<aside class="entry-author profile-box">
+		<h4><i class="icon-user"></i> この記事を書いた部員</h4>
+		<?php
+			// $post->post_authorが記事を投稿しているユーザのIDらしい
+			$uid   = $post->post_author;
+			$user  = get_userdata( $uid );
+			$grade = get_user_grade( $user );
+		?>
+		<?php /* アバター */ echo get_avatar( $uid ); ?>
+		<div class="profile">
+			<div class="name-grade-box">
+				<h5 class="name"><?php echo $user->display_name; ?></h5>
+				<div class="grade"><?php echo $grade; ?></div>
+				<div class="social-accounts">
+					<?php if ( $user->twitter ): // Twitterアカウント ?>
+						<span class="social-account"><i class="icon-twitter"></i><a href="//twitter.com/<?php echo $user->twitter ?>"><?php echo $user->twitter ?></a></span>
+					<?php endif; ?>
+					<?php if ( $user->facebook ): // Facebookアカウント ?>
+						<span class="social-account"><i class="icon-facebook-squared"></i><a href="//www.facebook.com/<?php echo $user->facebook ?>"><?php echo $user->facebook ?></a></span>
+					<?php endif; ?>
+					<?php if ( $user->github ): // GitHubアカウント ?>
+						<span class="social-account"><i class="icon-github"></i><a href="//github.com/<?php echo $user->github ?>"><?php echo $user->github ?></a></span>
+					<?php endif; ?>
+					<?php if ( $user->tumblr ): // Tumblrアカウント ?>
+						<span class="social-account"><i class="icon-tumblr-squared"></i><a href="//<?php echo $user->tumblr ?>.tumblr.com/"><?php echo $user->tumblr ?></a></span>
+					<?php endif; ?>
+					<?php if ( $user->hatena ): // Hatenaアカウント ?>
+						<span class="social-account"><i class="icon-hatena"></i><a href="//profile.hatena.ne.jp/<?php echo $user->hatena ?>/"><?php echo $user->hatena ?></a></span>
+					<?php endif; ?>
+				</div>
+			</div>
+			<?php if ( $user->user_description ): ?>
+				<p><?php echo $user->user_description; ?>
+			<?php else: ?>
+				<p class="descrip-none">自己紹介文はありません</p>
+			<?php endif; ?>
+			<div class="write-post-list-link">
+				<?php if ( count_user_posts($uid) > 0 ): ?>
+					<i class="icon-article"></i><a href="<?php echo home_url('/')."blog/author/".$user->user_login ?>">この部員が他に書いた記事(<?php echo count_user_posts($uid); ?>件)</a>
+				<?php else: ?>
+					この部員はまだ記事を書いていません
+				<?php endif; ?>
+			</div>
+		</div>
+	</aside>
+	<?php endif; ?>
+
 	<div class="entry-meta">
 		<span class="date">
 			<i class="icon-clock"></i>
