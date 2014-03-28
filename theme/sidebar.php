9a729bfee44be7c211e96550c1a786a4ef3ed0e3
windyakin <windyakin@gmail.com>
2014-03-28 22:21:51 +0900

    サイドバーに入部届のリンクを追加

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -8,7 +8,7 @@
 <div id="sidebar" class="col_3">
 	<?php if (is_page() || get_post_type() == 'active' || get_post_type() == 'product'): ?>
 		<div class="picbox shadow" id="square"><a href="<?php bloginfo('url');?>/radio"><img src="<?php bloginfo('template_url'); ?>/img/sysken_radio.png" width="222" height="222"></a></div>
-
+		<div class="picbox shadow" id="square"><a href="<?php bloginfo('url');?>/join-us/"><img src="<?php bloginfo('template_url'); ?>/img/join.png" width="222" height="222"></a></div>
 	<?php else : ?>
 		<?php dynamic_sidebar('sidebar-1'); ?>
 	<?php endif; ?>
